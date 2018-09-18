256a4e0ce4b022d17bb41ab6b3a965e047acd107
luckysym <powdog@yeah.net>
2018-09-18 17:32:42 +0800

    工具程序lsnetaddr，实现根据hostname获取IP地址，以及枚举本地IP地址列表功能。

--- tools/lsnetaddr/lsnetaddr_main.cpp
+++ tools/lsnetaddr/lsnetaddr_main.cpp
@@ -1,20 +1,56 @@
-
 #include <taurus/net/network.h>
 
-void print_all_addresses() ;
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-int main(int argc, char **argv) {
-    print_all_addresses();
-    return 0;
+void print_help(const char * program);
+void list_host_addresses(const char * hostname);
+void list_local_addresses();
+
+int main(int argc, char *argv[]) {
+    int opt;
+    while ((opt = getopt(argc, argv, "lhn:")) != -1) {
+        switch (opt) {
+            case 'n':
+                list_host_addresses(optarg);
+                break;
+            case 'h':
+                print_help(argv[0]);
+                break;
+            case 'l':
+                list_local_addresses();
+                break;
+        }
+    }
+
+    /* Other code omitted */
+    exit(EXIT_SUCCESS);
 }
 
-void print_all_addresses() {
-    using namespace taurus::net;
+void print_help(const char * program) {
+    printf("%s <options>\n\n", program);
+}
 
-    InetAddress::Vector vec;
-    vec.reserve(32);
-    ssize_t r = InetAddress::GetAllByName(vec, nullptr); 
-    for( ssize_t i = 0 ; i < r; ++i) {
-        printf("%s\n", vec[i]->ToString().c_str());
+void list_host_addresses(const char * hostname) {
+    using namespace taurus::net;
+    InetAddress::Vector addrlist;
+    size_t n = InetAddress::GetAllByName(addrlist, hostname);
+    printf("%ld ip addresses found for hostname %s. \n", n, hostname);
+    for ( size_t i = 0; i < n; ++i ) 
+        printf("%s\n", addrlist[i]->ToString().c_str());
+}
+
+void list_local_addresses() {
+    using namespace taurus::net;
+    InetAddress::Vector addrlist;
+    size_t n = InetAddress::GetLocalHost(addrlist);
+    if ( n > 0 ) {
+        for ( size_t i = 0; i < n; ++i ) 
+        printf("%s\n", addrlist[i]->ToString().c_str());
+    } else {
+        printf("%ld ip addresses found in localhost. \n", n);
     }
+     
+    
 }
