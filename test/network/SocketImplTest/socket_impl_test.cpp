73b86f3d81d081ea292a8cbd206575d54e42cef8
luckysym <powdog@yeah.net>
2018-11-03 01:23:33 +0800

    新增单元测试，SocketImpl::Connect等。

--- test/network/SocketImplTest/socket_impl_test.cpp
+++ test/network/SocketImplTest/socket_impl_test.cpp
@@ -12,69 +12,124 @@ using namespace taurus::net;
 using namespace std;
 
 class SocketImpTest : public CppUnit::TestFixture {
-    // 声明一个TestSuite
     CPPUNIT_TEST_SUITE( SocketImpTest );
-    // 添加测试用例到TestSuite, 定义新的测试用例需要在这儿声明一下
     CPPUNIT_TEST( testServerSocket );
-    // TestSuite声明完成
+    CPPUNIT_TEST( testClientSocketInvalid );
+    CPPUNIT_TEST( testClientSocketValid );
     CPPUNIT_TEST_SUITE_END();
 
 private:
-    SocketImpl::Ptr m_ptrSockImpl;
-
+    SocketImpl::Ptr m_ptrSocketImpl;
+    
 public:
     SocketImpTest() {
         cout<<"construct"<<endl;
     }
 
     void setUp () { 
-        CreateSocket();
-    }
-    void tearDown() {
-        CloseSocket();
+        CreateSocket(m_ptrSocketImpl);
+        // CreateClientSocket();
     }
 
+    void tearDown() {
+        // CloseClientSocket();
+        CloseSocket(m_ptrSocketImpl);
+    }
+
+    // 测试创建一个服务端socket并执行监听
     void testServerSocket() {
-        this->BindSocket();
-        this->ListenSocket();
-        this->GetLocalAddress();
+        this->BindSocket(m_ptrSocketImpl);
+        this->ListenSocket(m_ptrSocketImpl);
+        this->GetLocalAddress(m_ptrSocketImpl);
+    }
+
+    // 测试创建一个客户端socket并连接一个无效地址
+    void testClientSocketInvalid() {
+        this->ConnectSocketInvalid(m_ptrSocketImpl);
+    }
+
+    void testClientSocketValid() {
+        this->ConnectSocketLocal22(m_ptrSocketImpl);
+        this->GetRemoteAddress22(m_ptrSocketImpl);
+        this->GetClientLocalAddress(m_ptrSocketImpl);
+        this->ShutdownSocket(m_ptrSocketImpl);
     }
 
 protected:
-    void CreateSocket() {
-        m_ptrSockImpl = std::make_shared<SocketImpl>();
-        printf("%p\n", m_ptrSockImpl.get());
-        CPPUNIT_ASSERT( m_ptrSockImpl != nullptr );
+    void CreateSocket(SocketImpl::Ptr &ptrSocket) {
+        ptrSocket.reset(new SocketImpl());
+        printf("Server Socket Ptr: %p\n", ptrSocket.get());
+        CPPUNIT_ASSERT( ptrSocket != nullptr );
 
         std::string errinfo;
-        CPPUNIT_ASSERT( m_ptrSockImpl->Create(Protocol::Tcp4, errinfo) );
-        CPPUNIT_ASSERT( m_ptrSockImpl->Fd() > 0 );
-        CPPUNIT_ASSERT( m_ptrSockImpl->State() == SocketImpl::SOCK_STATE_CREATED );
-        cout<<"socket created, fd: "<<m_ptrSockImpl->Fd()<<endl;
+        CPPUNIT_ASSERT( ptrSocket->Create(Protocol::Tcp4, errinfo) );
+        CPPUNIT_ASSERT( ptrSocket->Fd() > 0 );
+        CPPUNIT_ASSERT( ptrSocket->State() == SocketImpl::SOCK_STATE_CREATED );
+        cout<<"socket created, fd: "<<ptrSocket->Fd()<<endl;
     }
-    void CloseSocket() {
+
+    void CloseSocket(SocketImpl::Ptr &ptrSocket) {
         std::string errinfo;
-        CPPUNIT_ASSERT( m_ptrSockImpl->Close(errinfo));
-        CPPUNIT_ASSERT( m_ptrSockImpl->Fd() == INVALID_SOCKET );
-        CPPUNIT_ASSERT( m_ptrSockImpl->State() == SocketImpl::SOCK_STATE_CLOSED );
+        cout<<"Socket Closed: "<<ptrSocket->Fd()<<endl;
+        CPPUNIT_ASSERT( ptrSocket->Close(errinfo));
+        CPPUNIT_ASSERT( ptrSocket->Fd() == INVALID_SOCKET );
+        CPPUNIT_ASSERT( ptrSocket->State() == SocketImpl::SOCK_STATE_CLOSED );
     }
-    void BindSocket () {
+
+    void BindSocket (SocketImpl::Ptr &ptrSocket) {
         std::string errinfo;
-        CPPUNIT_ASSERT( m_ptrSockImpl->Bind(Inet4Address(), 10024, errinfo));
+        CPPUNIT_ASSERT( ptrSocket->Bind(Inet4Address(), 10024, errinfo));
     }
-    void ListenSocket() {
+    void ListenSocket(SocketImpl::Ptr &ptrSocket) {
         std::string errinfo;
-        CPPUNIT_ASSERT( m_ptrSockImpl->Listen(SOMAXCONN, errinfo));
-        CPPUNIT_ASSERT( m_ptrSockImpl->State() == SocketImpl::SOCK_STATE_OPEN );
+        CPPUNIT_ASSERT( ptrSocket->Listen(SOMAXCONN, errinfo));
+        CPPUNIT_ASSERT( ptrSocket->State() == SocketImpl::SOCK_STATE_OPEN );
     }
-    void GetLocalAddress() {
+    void GetLocalAddress(SocketImpl::Ptr &ptrSocket) {
         std::string errinfo;
-        const InetSocketAddress * paddr = m_ptrSockImpl->GetLocalAddress(errinfo);
+        const InetSocketAddress * paddr = ptrSocket->GetLocalAddress(errinfo);
         CPPUNIT_ASSERT(paddr);
         CPPUNIT_ASSERT(paddr->GetPort() == 10024);
         cout<<paddr->ToString()<<endl;
     }
 
+    void ConnectSocketInvalid(SocketImpl::Ptr &ptrSocket) {
+        std::string errinfo;
+        Inet4Address address("10.10.10.10", errinfo);
+        CPPUNIT_ASSERT( errinfo.empty() );
+        CPPUNIT_ASSERT(!ptrSocket->Connect(address, 10024, errinfo));
+        cout<<"Connect error: "<<errinfo<<endl;
+        CPPUNIT_ASSERT( ptrSocket->State() == SocketImpl::SOCK_STATE_CREATED);
+    }
+
+    void ConnectSocketLocal22(SocketImpl::Ptr &ptrSocket) {
+        std::string errinfo;
+        Inet4Address address("127.0.0.1", errinfo);
+        CPPUNIT_ASSERT( errinfo.empty() );
+        CPPUNIT_ASSERT( ptrSocket->Connect(address, 22, errinfo));
+        CPPUNIT_ASSERT( ptrSocket->State() == SocketImpl::SOCK_STATE_OPEN);
+    }
+
+    void GetRemoteAddress22(SocketImpl::Ptr &ptrSocket) {
+        std::string errinfo;
+        const InetSocketAddress * paddr = ptrSocket->GetRemoteAddress(errinfo);
+        CPPUNIT_ASSERT(paddr);
+        CPPUNIT_ASSERT(paddr->GetPort() == 22);
+        cout<<paddr->ToString()<<endl;
+    }
+
+    void GetClientLocalAddress(SocketImpl::Ptr &ptrSocket) {
+        std::string errinfo;
+        const InetSocketAddress * paddr = ptrSocket->GetLocalAddress(errinfo);
+        CPPUNIT_ASSERT(paddr);
+        cout<<paddr->ToString()<<endl;
+    }
+
+    void ShutdownSocket(SocketImpl::Ptr &ptrSocket) {
+        std::string errinfo;
+        CPPUNIT_ASSERT( ptrSocket->ShutdownInput(errinfo) );
+        CPPUNIT_ASSERT( ptrSocket->ShutdownOutput(errinfo) );
+    }
 }; // end class SocketImpTest
 
 // CPPUNIT_TEST_SUITE_NAMED_REGISTRATION( SocketImpTest, "alltest" );
